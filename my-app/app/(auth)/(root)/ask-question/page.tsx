9afab0f26d42ae48be075a586bebfc831598f2fc
sham savvasher <yoyoshamsavvasher@gmail.com>
2024-11-13 00:23:52 +0530

    first commit

---
+++ my-app/app/(auth)/(root)/ask-question/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Askquestion
+ = () => {
+  return (
+    <div>
+      Askquestion
+    </div>
+  )
+}
+
+export default Askquestion
+
---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex-center h1-bold">Heloo</h1>
+    </div>
+  );
+}
