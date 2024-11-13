fc4f1e47b33b279c46e722846086d9c4a9fa45cb
sham savvasher <yoyoshamsavvasher@gmail.com>
2024-11-14 00:26:20 +0530

    add assets and layout

--- my-app/app/(auth)/(root)/(home)/page.tsx
+++ my-app/app/(auth)/(root)/(home)/page.tsx
@@ -2,7 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl ="/"/>
+      <UserButton afterSignOutUrl="/" />
+
     </div>
   )
 }
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -4,9 +4,25 @@ import React from 'react'
 import { Inter, Space_Grotesk } from 'next/font/google';
 import type {Metadata} from 'next'
 
+const inter = Inter(
+  {
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500','600', '700', '800', '900'],
+    variable:'--font-inter'
+  }
+  
+)
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: [ '300', '400', '500','600', '700'],
+  variable:'--font-space-grotesk'
+})
 export const metadata: Metadata = {
   title: 'DevFlow',
-  description: 'A community-driven platform for asking and answring programming question.',
+  description: 'A community-driven platform for asking and answring programming questions Get hep, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app developement, algorithms, data structures, and more.',
+  icons: {
+    icon:'/assets/images/site-logo.svg'
+  }
 }  
 export default function RootLayout({
   children,
@@ -14,9 +30,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 
+            'primary-gradient',
+          footerAction:
+          'primary-text-gradient hover:text-primary-500',
+            
+          
+      }
+    }}>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable}${spaceGrotesk}`}>
+          {children}</body>
       </html>
     </ClerkProvider>
   )
